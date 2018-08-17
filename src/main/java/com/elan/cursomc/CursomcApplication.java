44bccc354a3c7125979fbee370de23def64a3b47
Elan Fraga <elanfraga@gmail.com>
2018-08-16 23:28:23 -0300

    rest testado

--- src/main/java/com/elan/cursomc/CursomcApplication.java
+++ src/main/java/com/elan/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.elan.cursomc.cursomc;
+package com.elan.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
