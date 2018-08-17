6fb889c897a5b34328f2dc68efceeebe18d75bab
Elan Fraga <elan.fraga@zaal.com.br>
2018-08-17 16:23:06 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/elan/cursomc/servicies/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.elan.cursomc.servicies;
+
+import com.elan.cursomc.domain.Categoria;
+import com.elan.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj =  repo.findById(id);
+        return obj.orElse(null);
+    }
+}
