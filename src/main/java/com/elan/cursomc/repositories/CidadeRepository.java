26e0325517208b92f4da80546fb708b9cba0560d
Elan Fraga <elanfraga@gmail.com>
2018-08-17 23:36:34 -0300

    Estado e Cidade

---
+++ src/main/java/com/elan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.elan.cursomc.repositories;
+
+import com.elan.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository  extends JpaRepository<Cidade, Integer> {
+}
