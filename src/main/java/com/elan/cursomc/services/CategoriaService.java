52eff2caee11f8cf2daef722e3af215bd1d11827
Elan Fraga <elanfraga@gmail.com>
2018-08-20 16:11:33 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/elan/cursomc/services/CategoriaService.java
+++ src/main/java/com/elan/cursomc/services/CategoriaService.java
@@ -1,7 +1,8 @@
         package com.elan.cursomc.services;
 
         import com.elan.cursomc.domain.Categoria;
-import com.elan.cursomc.repositories.CategoriaRepository;
+        import com.elan.cursomc.dto.CategoriaDTO;
+        import com.elan.cursomc.repositories.CategoriaRepository;
 import com.elan.cursomc.services.exceptions.DataIntegrityException;
 import com.elan.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,5 +56,8 @@ public class CategoriaService {
         return repo.findAll(pageRequest);
     }
 
+    public Categoria fromDTO(CategoriaDTO objDto){
+        return new Categoria(objDto.getId(), objDto.getNome());
+    }
 
 }
