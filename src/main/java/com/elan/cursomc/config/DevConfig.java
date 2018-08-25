8df7782e86f01817005ff8a3eef9d3b9d0eb5afe
Elan Fraga <elanfraga@gmail.com>
2018-08-25 16:24:55 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/elan/cursomc/config/DevConfig.java
+++ src/main/java/com/elan/cursomc/config/DevConfig.java
@@ -2,7 +2,6 @@ package com.elan.cursomc.config;
 
 import com.elan.cursomc.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,22 +9,14 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
     @Bean
     public  boolean instatiateDatabase() throws ParseException {
-
-        if (!"create".equals(strategy)){
-            return false;
-        }
-
         dbService.instantiateTestDatabase();
         return true;
     }
