9cab06eb8aba71dd469a5ce2ad2201d013cd8b54
Elan Fraga <elanfraga@gmail.com>
2018-08-25 22:39:01 -0300

    Implementando toString de Pedido

--- src/main/java/com/elan/cursomc/config/TestConfig.java
+++ src/main/java/com/elan/cursomc/config/TestConfig.java
@@ -2,12 +2,12 @@ package com.elan.cursomc.config;
 
 import com.elan.cursomc.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.text.ParseException;
+
 
 @Configuration
 @Profile("test")
@@ -16,16 +16,8 @@ public class TestConfig {
     @Autowired
     private DBService dbService;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
     @Bean
-    public  boolean instatiateDatabase() throws ParseException {
-
-        if (!"create".equals(strategy)){
-            return false;
-        }
-
+    public boolean instantiateDatabase() throws ParseException {
         dbService.instantiateTestDatabase();
         return true;
     }
