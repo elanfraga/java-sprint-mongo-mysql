b9103ca122cca8eb53d2621eaefc2daa661827a8
Elan Fraga <elanfraga@gmail.com>
2018-08-26 22:34:55 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/elan/cursomc/services/EmailService.java
@@ -0,0 +1,11 @@
+package com.elan.cursomc.services;
+
+import com.elan.cursomc.domain.Pedido;
+import org.springframework.mail.SimpleMailMessage;
+
+public interface EmailService {
+
+    void sendOrderConfirmationEmail(Pedido obj);
+
+    void sendEmail(SimpleMailMessage msg);
+}
